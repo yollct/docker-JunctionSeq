fe8e86f6a629e6fe6b679c3acdd99130dc7bdb67
Chit Tong Lio <chit-tong.lio@wzw.tum.de>
2023-03-16 11:25:18 +0100

    Add files via upload

---
+++ junctionseq.R
@@ -0,0 +1,52 @@
+
+library(tidyverse)
+library(JunctionSeq)
+library(DESeq2)
+biocpar <- 30
+#source("/nfs/home/students/chit/is_benchmark/scripts/junctionseq_code.R")
+
+args <- commandArgs(trailingOnly=TRUE)
+outdir <- args[1]
+con1 <- args[2]
+con2 <- args[3]
+con <- c(con1, con2)
+print("comparing:")
+print(con)
+# outdir <- "/nfs/scratch/chit/simulated_real/single2/"
+# path <- "/nfs/home/students/chit/is_benchmark"
+# meta <- "/nfs/scratch/chit/simulated_real/single2/meta.txt"
+# index <- "/nfs/scratch/chit/ref/ens98_star_rsem"
+
+if (!file.exists(paste0(outdir, sprintf("/results/jseq_%s_%s_/jscs.RData", con1, con2)))){
+    metadata <- read.csv("/MOUNT/meta.txt", sep="\t")
+
+    metadata <- metadata %>% dplyr::filter(group %in% con)
+    metadata$sample_id <- lapply(metadata$sample_id, function(x){gsub(".sra", "", x)}) %>% unlist()
+    countFiles <- paste0(outdir, "/qort/", metadata$sample_id,
+    "/QC.spliceJunctionAndExonCounts.forJunctionSeq.txt.gz")
+    metadata$sample_id <- lapply(metadata$sample_id, function(x){as.character(x)}) %>% unlist
+
+    jscs <- runJunctionSeqAnalyses(sample.files = countFiles,
+                                    sample.names = metadata$sample_id,
+                                    condition=factor(metadata$group),
+                                    flat.gff.file = paste0("/MOUNT", "/JunctionSeq.flat.gff.gz"),
+                                    nCores = biocpar,
+                                    analysis.type = "junctionsAndExons"
+                                    )
+
+    #Generate the size factors and load them into the JunctionSeqCountSet:
+    jscs <- estimateJunctionSeqSizeFactors(jscs)
+    jscs <- estimateJunctionSeqDispersions(jscs, nCores=biocpar)
+    jscs <- fitJunctionSeqDispersionFunction(jscs)
+    jscs <- testForDiffUsage(jscs, nCores=biocpar)
+    jscs <- estimateEffectSizes(jscs, nCores=biocpar)
+
+    if (!file.exists(paste0(outdir, sprintf("/results/jseq_%s_%s_/", con1, con2)))){
+        dir.create(paste0(outdir, sprintf("/results/jseq_%s_%s_/", con1, con2)))
+    }
+
+    writeCompleteResults(jscs,
+    outfile.prefix=paste0(outdir, sprintf("/results/jseq_%s_%s_/", con1, con2)),
+    save.jscs = TRUE)
+
+}
